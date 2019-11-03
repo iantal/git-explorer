01485933c1676a89ff78a90140cbcbc931774f6b
iantal <antal.micky@yahoo.com>
2019-11-03 15:56:10 +0200

    Add Spring Boot initial setup

---
+++ core/src/test/java/com/gitexplorer/core/GitExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitexplorer.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
