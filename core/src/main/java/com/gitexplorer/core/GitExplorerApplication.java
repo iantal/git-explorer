939de21ef7c72909ef79004923a2f31ceb6def6e
iantal <antal.micky@yahoo.com>
2019-11-03 21:37:51 +0200

    Add endpoint for querying top repositories

--- core/src/main/java/com/gitexplorer/core/GitExplorerApplication.java
+++ core/src/main/java/com/gitexplorer/core/GitExplorerApplication.java
@@ -1,13 +1,24 @@
 package com.gitexplorer.core;
 
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.client.GitHubRequest;
+import org.eclipse.egit.github.core.client.GitHubResponse;
+import org.eclipse.egit.github.core.service.GitHubService;
+import org.eclipse.egit.github.core.service.RepositoryService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class GitExplorerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitExplorerApplication.class, args);
-	}
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(GitExplorerApplication.class, args);
+    }
 
 }
---
+++ core/src/main/java/com/gitexplorer/core/data/TrendingReport.java
@@ -0,0 +1,25 @@
+package com.gitexplorer.core.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TrendingReport implements Serializable {
+    private String author;
+    private String name;
+    private String avatar;
+    private String url;
+    private String description;
+    private String language;
+    private String languageColor;
+    private int stars;
+    private int forks;
+    private int currentPeriodStars;
+    private List<TrendingReportContributor> builtBy;
+}
